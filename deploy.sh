45555ea9f2a34223d63f2d79ef4d7026402e8bce
rickyandhi <ricky10andika@gmail.com>
2023-09-03 14:03:12 +0700

    deploy function script

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,43 @@
-gcloud functions deploy hello \
---gen2 \
---region=us-central1 \
---runtime=nodejs20 \
---entry-point=index \
---trigger-http \
---allow-unauthenticated \
---source=./dist/hello \
---memory=128Mi \
---max-instances=2
+#!/bin/bash
 
-gcloud functions deploy index \
---gen2 \
---region=us-central1 \
---runtime=nodejs20 \
---entry-point=index \
---trigger-http \
---allow-unauthenticated \
---source=./dist/index \
---memory=128Mi \
---max-instances=2
+if [ "$1" = "all" ] || [ "$1" = "hello" ]; then
+  echo "deploying hello route"
+  gcloud functions deploy hello \
+    --gen2 \
+    --region=us-central1 \
+    --runtime=nodejs20 \
+    --entry-point=index \
+    --trigger-http \
+    --allow-unauthenticated \
+    --source=./dist/routes/hello \
+    --memory=128Mi \
+    --max-instances=2
+fi
+
+if [ "$1" = "all" ] || [ "$1" = "index" ]; then
+  echo "deploying index route"
+  gcloud functions deploy index \
+    --gen2 \
+    --region=us-central1 \
+    --runtime=nodejs20 \
+    --entry-point=index \
+    --trigger-http \
+    --allow-unauthenticated \
+    --source=./dist/routes/index \
+    --memory=128Mi \
+    --max-instances=2
+fi
+
+if [ "$1" = "all" ] || [ "$1" = "onTestMessage" ]; then
+  echo "deploying onTestMessage event handler"
+  gcloud functions deploy onTestMessage \
+    --gen2 \
+    --region=us-central1 \
+    --runtime=nodejs20 \
+    --entry-point=index \
+    --retry \
+    --trigger-topic=on-test-message \
+    --source=./dist/events/onTestMessage \
+    --memory=128Mi \
+    --max-instances=2
+fi
